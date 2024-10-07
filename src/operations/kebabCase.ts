1847eddb9ec11a3056a04d1722ced0090356f74f
Roy Chanunkha <roychanunkha@gmail.com>
2024-10-07 09:05:05 +0200

    adding more cases

--- src/operations/kebabCase.ts
+++ src/operations/kebabCase.ts
@@ -9,6 +9,53 @@
  * spaces with hyphens and converting the entire string to lowercase.
  *
  */
-export const kebabCase = (str: string): string => {
+const kebabCase = (str: string): string => {
   return str.replace(/\s+/g, '-').toLowerCase();
 };
+
+/**
+ * Converts a string to pascal-case.
+ * @param {string} str - The input string to convert.
+ * @returns {string} The pascal-cased version of the input string.
+ * 
+ * @description
+ * This function converts a given string to pascal-case by replacing all
+ * spaces with an empty string and converting the first character of each
+ * word to uppercase.
+ */
+const pascalCase = (str: string): string => {
+  return str
+    .replace(/(\w)(\w*)/g, (g0, g1, g2) => g1.toUpperCase() + g2.toLowerCase())
+    .replace(/\s/g, '');
+}
+
+/**
+ * Converts a string to camel-case.
+ * @param {string} str - The input string to convert.
+ * @returns {string} The camel-cased version of the input string.
+ * @description
+ * This function converts a given string to camel-case by replacing all
+ * spaces with an empty string and converting the first character of each
+ * word to uppercase.
+ * 
+ */
+const camelCase = (str: string): string => {
+  return str
+    .replace(/(\w)(\w*)/g, (g0, g1, g2) => g1.toUpperCase() + g2.toLowerCase())
+    .replace(/\s/g, '');
+}
+
+/**
+ * Converts a string to snake-case.
+ * @param {string} str - The input string to convert.
+ * @returns {string} The snake-cased version of the input string.
+ * @description
+ * This function converts a given string to snake-case by replacing all
+ * spaces with an underscore and converting the entire string to lowercase.
+ */
+const snakeCase = (str: string): string => {
+  return str.replace(/\s+/g, '_').toLowerCase();
+}
+
+export { kebabCase, pascalCase, camelCase, snakeCase };
+
